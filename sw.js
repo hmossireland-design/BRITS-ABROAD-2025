0251798e1c874f5b563a9a40fbc480d11599f41c
hmossireland-design <hmossireland@gmail.com>
2025-12-11 06:52:19 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,7 @@
-const CACHE = 'brits-abroad-v1';
-const FILES = [
+const CACHE = 'brits-v1';
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll([
   '/BRITS-ABROAD-2025/',
   '/BRITS-ABROAD-2025/index.html',
-  '/BRITS-ABROAD-2025/manifest.json',
-  '/BRITS-ABROAD-2025/sw.js',
-  '/BRITS-ABROAD-2025/icons/icon-192.svg',
-  '/BRITS-ABROAD-2025/icons/icon-512.svg'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(FILES))
-  );
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
+  '/BRITS-ABROAD-2025/manifest.json'
+]))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
