fd5b7b2a4515c4b417d5f334a5504e5c2615db02
hmossireland-design <hmossireland@gmail.com>
2025-12-11 05:06:11 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,34 @@
-const CACHE = 'brits-abroad-v1';
+const CACHE = "brits-abroad-v1";
 
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE).then(cache => {
       return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json'
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icons/icon-192.png",
+        "./icons/icon-512.png"
       ]);
     })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key =>
+          key !== CACHE ? caches.delete(key) : null
+        )
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
