a46099e3dbd487692ebef79af9ae65e794ac01e6
hmossireland-design <hmossireland@gmail.com>
2025-12-11 06:23:50 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,7 @@
-const CACHE_NAME = 'brits-abroad-v1';
-const FILES_TO_CACHE = [
-  './index.html',
-  './manifest.json',
-  './sw.js',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(key => { if (key !== CACHE_NAME) return caches.delete(key); })
-    ))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
+<script>
+if('serviceWorker' in navigator){
+  navigator.serviceWorker.register('/BRITS-ABROAD-2025/sw.js')
+    .then(reg => console.log('SW registered', reg))
+    .catch(err => console.warn('SW failed', err));
+}
+</script>
