ad85896aebf4913e37c50b70d8dfe0b6ef5ff4dd
hmossireland-design <hmossireland@gmail.com>
2025-12-11 05:36:55 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,30 @@
-const CACHE = "brits-abroad-v1";
+const CACHE_NAME = 'brits-abroad-v1';
+const FILES_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  './sw.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./icons/icon-192.png",
-        "./icons/icon-512.png"
-      ]);
-    })
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key =>
-          key !== CACHE ? caches.delete(key) : null
-        )
-      )
-    )
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => { if (key !== CACHE_NAME) return caches.delete(key); })
+    ))
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
